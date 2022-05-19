4bd2600b33105d0adcc94bdbc2b4e96187d21fcf
Joseph Estevez <joseph.estevez@protonmail.com>
2022-05-19 11:12:09 -0400

    add a basic layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import { ReactElement } from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Paint It Black</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
