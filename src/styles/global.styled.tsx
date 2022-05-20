1f0739bef32fc061d6fdc18dbdd3acc4a82d2d66
Joseph Estevez <joseph.estevez@protonmail.com>
2022-05-19 20:55:54 -0400

    add Ubunto Mono font face

--- src/styles/global.styled.tsx
+++ src/styles/global.styled.tsx
@@ -1,9 +1,21 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Ubuntu Mono' ;
+    src: url('static/fonts/Ubuntu Mono Nerd Font Complete.ttf') format(truetype);
+  }
+
   *{
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  html,body{
+    font-family:  Ubuntu Mono, -apple-system, BlinkMacSystemFont, 'Segoe UI', 
+      Roboto, Oxygen, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    line-height: 0.8;
   }
 `
